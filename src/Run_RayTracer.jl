22e636e6391e1ad574b5bf5f18f78e87d52840e3
Samuel Witte <witte.sam@gmail.com>
2021-05-07 13:40:16 +0200

    single file code

--- src/Run_RayTracer.jl
+++ src/Run_RayTracer.jl
@@ -1,6 +1,7 @@
 using LinearAlgebra
 using Statistics
 using NPZ
+using NLsolve
 using SpecialFunctions
 
 include("../src/photon_raytrace.jl")
@@ -16,4 +17,19 @@ NS_vel = [0 0 200.0] ./ 2.998e5;
 n_times = 2;
 t_list = LinRange(0.0, 2.0 * π / ωPul, n_times)
 
-main_runner(Mass_a, Ax_g, θm, ωPul, B0, rNS, Mass_NS, t_list; ode_err=1e-7, CLen_Scale=true, v_NS=NS_vel)
+phiVs = 20
+thetaVs = 20
+ln_tend = 20
+threshold = 0.0001
+sve = true;
+
+file_tag = "TEST_"
+RadApprox = false
+
+
+for i in 1:n_times
+    surface_solver(Mass_a, θm, ωPul, B0, rNS, t_list[i], NS_vel; nsteps=10, ln_tstart=-15, ln_tend=ln_tend, ode_err=1e-10, phiVs=phiVs, thetaVs=thetaVs, threshold=threshold, sve=sve);
+end
+
+main_runner(Mass_a, Ax_g, θm, ωPul, B0, rNS, Mass_NS, t_list; ode_err=1e-7, CLen_Scale=true, v_NS=NS_vel, file_tag=file_tag, RadApprox=RadApprox)
+period_average(Mass_a, Ax_g, θm, ωPul, B0, rNS, Mass_NS, t_list; ode_err=1e-7, CLen_Scale=true, v_NS=NS_vel, file_tag=file_tag, RadApprox=RadApprox)
--- src/photon_raytrace.jl
+++ src/photon_raytrace.jl
@@ -78,6 +78,26 @@ function func!(du, u, Mvars, lnt)
     end
 end
 
+function func_axion!(du, u, Mvars, lnt)
+    @inbounds begin
+        t = exp.(lnt)
+        x = u[:,1:3]
+        v = u[:,4:6]
+        
+        r = sqrt.(sum(x .* x, dims=2))
+        
+        xhat = x ./ r
+        massL = ones(length(r))
+        if sum(r .< 10) > 0
+            massL[r .< 10] .= 1.0 .* (r[r .< 10] ./ 10.0) .^ 3;
+        end
+        
+        G = 132698000000.0 # km M_odot^-1 * (km/s)^2
+        du[:,1:3] = -v .* 2.998e5 .* t;  # v is km/s, x in km, t [s]
+        du[:,4:6] = G .* massL ./ r.^2 .* xhat ./ 2.998e5 .* t; # unitless, assume 1M NS
+    end
+end
+
 
 # propogate photon module
 function propagate(ω, x0::Matrix, k0::Matrix,  nsteps::Int, Mvars::Array, NumerP::Array)
@@ -95,6 +115,155 @@ function propagate(ω, x0::Matrix, k0::Matrix,  nsteps::Int, Mvars::Array, Numer
     dt = cat([Array(u)[:, 7] for u in sol.u]..., dims = 2);
     
     return x, k, dt
+end
+
+function propagateAxion(x0::Matrix, k0::Matrix, nsteps::Int, NumerP::Array)
+    ln_tstart, ln_tend, ode_err = NumerP
+    tspan = (ln_tstart, ln_tend)
+    saveat = (tspan[2] - tspan[1]) / (nsteps-1)
+
+    # u0 = cu([x0 k0])
+    u0 = ([x0 k0])
+    
+    probAx = ODEProblem(func_axion!, u0, tspan, [ln_tstart], reltol=ode_err*1e-4, abstol=ode_err, maxiters=1e6);
+    sol = solve(probAx, Tsit5(), saveat=saveat);
+    
+    
+    x = cat([u[:, 1:3] for u in sol.u]...,dims=3);
+    v = cat([u[:, 4:6] for u in sol.u]...,dims=3);
+    
+    return x, v
+    
+end
+
+
+function Find_Conversion_Surface(Ax_mass, t_in, θm, ω, B0, rNS; thetaVs=100, phiVs=100)
+
+    # rayT = RayTracer;
+    rL = 10 .^ LinRange(log10.(rNS), 3, 5000)
+    θL = LinRange(0.02 , π - 0.02, thetaVs)
+    ϕL = LinRange(0.02, 2 * π - 0.02, phiVs)
+    finalVec = zeros(length(θL)*length(ϕL), 3)
+
+    indx = 1
+    for i in 1:length(θL)
+        for k in 1:length(ϕL)
+
+            x0 = [rL .* sin.(θL[i]) .* cos.(ϕL[k]) rL .* sin.(θL[i]) .* sin.(ϕL[k]) rL .* cos.(θL[i])];
+            ωpL = GJ_Model_ωp_vec(x0, t_in, θm, ω, B0, rNS)
+
+            cx_list = get_crossings(log.(ωpL) .- log.(Ax_mass))
+            cross = apply(cx_list, rL)
+            if length(cx_list.i1) != 0
+                finalVec[indx, :] .= [θL[i], ϕL[k], cross[1]]
+            else
+                finalVec[indx, :] .= [θL[i], ϕL[k], 0.0]
+            end
+            indx += 1
+        end
+    end
+    return finalVec
+end
+
+
+function Get_Normal_CosTheta(x_list, v_list, Ax_mass, t_in, θm, ω, B0, rNS)
+    
+    r = sqrt.(sum(x_list .^2, dims=2))
+    ϕ = atan.(view(x_list, :, 2), view(x_list, :, 1))
+    θ = acos.(view(x_list, :, 3) ./ r)
+    
+    
+    ctheta = zeros(length(θ))
+    shift = 0.001
+    
+    for i in 1:length(θ)
+        rL = LinRange(r[i] ./ 4, r[i].*4, 5000);
+        x0 = [rL .* sin.(θ[i] ) .* cos.(ϕ[i]) rL .* sin.(θ[i] ) .* sin.(ϕ[i]) rL .* cos.(θ[i])];
+        ωpL = GJ_Model_ωp_vec(x0, t_in, θm, ω, B0, rNS);
+        cx_list = get_crossings(log.(ωpL) .- log.(Ax_mass));
+        cross = apply(cx_list, rL);
+        rMean = cross[1]
+        xMEAN = [rMean .* sin.(θ[i]) .* cos.(ϕ[i]) rMean .* sin.(θ[i]) .* sin.(ϕ[i]) rMean .* cos.(θ[i])];
+        
+        
+        
+        x0 = [rL .* sin.(θ[i] .+ shift) .* cos.(ϕ[i]) rL .* sin.(θ[i] .+ shift) .* sin.(ϕ[i]) rL .* cos.(θ[i] .+ shift)];
+        ωpL = GJ_Model_ωp_vec(x0, t_in, θm, ω, B0, rNS);
+        cx_list = get_crossings(log.(ωpL) .- log.(Ax_mass));
+        cross = apply(cx_list, rL);
+        if length(cx_list.i1) != 0
+            rx = cross[1]
+        else
+            rx = rMean
+        end
+        finalVec_N1 = [rx .* sin.(θ[i] .+ shift) .* cos.(ϕ[i]) rx .* sin.(θ[i] .+ shift) .* sin.(ϕ[i]) rx .* cos.(θ[i] .+ shift)];
+        
+        finalVec_N1 .-= xMEAN;
+        
+        x0 = [rL .* sin.(θ[i] .- shift) .* cos.(ϕ[i] .+ 2 .* shift) rL .* sin.(θ[i] .- shift) .* sin.(ϕ[i] + 2 .* shift) rL .* cos.(θ[i] .- shift)];
+        ωpL = GJ_Model_ωp_vec(x0, t_in, θm, ω, B0, rNS);
+        cx_list = get_crossings(log.(ωpL) .- log.(Ax_mass));
+        cross = apply(cx_list, rL);
+        if length(cx_list.i1) != 0
+            rx = cross[1]
+        else
+            rx = rMean
+        end
+        finalVec_N2 = [rx .* sin.(θ[i] .- shift) .* cos.(ϕ[i] + 2 .* shift) rx .* sin.(θ[i] .- shift) .* sin.(ϕ[i] .+ 2 .* shift) rx .* cos.(θ[i] .- shift)];
+        finalVec_N2 .-= xMEAN;
+        
+        val = [(finalVec_N1[2] .* finalVec_N2[3] .- finalVec_N1[3] .* finalVec_N2[2]) (finalVec_N1[3] .* finalVec_N2[1] .- finalVec_N1[1] .* finalVec_N2[3]) (finalVec_N1[1] .* finalVec_N2[2] .- finalVec_N1[2] .* finalVec_N2[1])];
+        val ./= sqrt.(sum(val.^2));
+        khat = v_list[i, :] ./  sqrt.(sum(v_list[i, :].^2));
+        
+        ctheta[i] = abs.(val[1] .* khat[1] .+ val[2] .* khat[2] .+ val[3] .* khat[3]);
+    end
+    
+    return ctheta
+end
+
+
+
+function ωNR_e(x, k, t, θm, ωPul, B0, rNS, gammaF)
+    #  GJ charge density, Magnetic field, non-relativstic e only
+    Bvec, ωpL = GJ_Model_vec(x, t, θm, ωPul, B0, rNS)
+
+    kmag = sqrt.(sum(k .* k, dims=2))
+    Bmag = sqrt.(sum(Bvec .* Bvec, dims=2))
+    ωp = sqrt.(sum(ωpL .* ωpL, dims=2))
+
+    cθ = sum(k .* Bvec, dims=2) ./ (kmag .* Bmag)
+    
+    cθTerm = (1.0 .- 2.0 .* cθ.^2)
+    
+    # abs not necessary, but can make calculation easier
+    return sqrt.(abs.(0.5 .* (kmag .^2 + ωp .^2 + sqrt.(abs.(kmag .^4 + ωp .^4 + 2.0 .* cθTerm .*kmag .^2 .* ωp .^2 )))))
+
+end
+
+
+function kNR_e(x, k, ω, t, θm, ωPul, B0, rNS, gammaF)
+    #  GJ charge density, Magnetic field, non-relativstic e only
+    Bvec, ωpL = GJ_Model_vec(x, t, θm, ωPul, B0, rNS)
+
+    kmag = sqrt.(sum(k .* k, dims=2))
+    Bmag = sqrt.(sum(Bvec .* Bvec, dims=2))
+    ωp = sqrt.(sum(ωpL .* ωpL, dims=2))
+
+    cθ = sum(k .* Bvec, dims=2) ./ (kmag .* Bmag)
+    
+    # abs not necessary, but can make calculation easier (numerical error can creap at lvl of 1e-16 for ctheta)
+    return sqrt.(((ω.^2 .-  ωp.^2) ./ (1 .- cθ.^2 .* ωp.^2 ./ ω.^2)))
+end
+
+function dk_dl(x0, k0, Mvars)
+    ω, Mvars2 = Mvars
+    θm, ωPul, B0, rNS, gammaF, t_start = Mvars2
+    ωErg = ω(x0, k0, t_start, θm, ωPul, B0, rNS, gammaF)
+    dkdr_grd = grad(kNR_e(seed(x0), k0, ωErg, t_start, θm, ωPul, B0, rNS, gammaF))
+    
+    dkdr_proj = abs.(sum(k0 .* dkdr_grd, dims=2) ./ sqrt.(sum(k0 .^ 2, dims=2)))
+    return dkdr_proj
 end
 
 
@@ -342,6 +511,165 @@ function ωFree(x, k, t, θm, ωPul, B0, rNS, gammaF)
     # assume simple case where ωp proportional to r^{-3/2}, no time dependence, no magnetic field
     return sqrt.(sum(k .* k, dims = 2) .+ 1e-40 .* sqrt.(sum(x .* x, dims=2)) ./ (rNS.^ 2) )
 end
+
+end
+
+
+
+
+function solve_vel_CS(θ, ϕ, r, NS_vel; guess=[0.1 0.1 0.1])
+    ff = sum(NS_vel.^2); # unitless
+    G = 132698000000.0 # km M_odot^-1 * (km/s)^2
+    GMr = G ./ r ./ (2.998e5 .^ 2); # unitless
+    rhat = [sin.(θ) .* cos.(ϕ) sin.(θ) .* sin.(ϕ) cos.(θ)]
+    
+    function f!(F, x)
+        vx, vy, vz = x
+        denom = ff .+ GMr .- sqrt.(ff) .* sum(x .* rhat);
+        
+        F[1] = (ff .* vx .+ sqrt.(ff) .* GMr .* rhat[1] .- sqrt.(ff) .* vx .* sum(x .* rhat)) ./ denom .- NS_vel[1]
+        F[2] = (ff .* vy .+ sqrt.(ff) .* GMr .* rhat[2] .- sqrt.(ff) .* vy .* sum(x .* rhat)) ./ denom .- NS_vel[2]
+        F[3] = (ff .* vz .+ sqrt.(ff) .* GMr .* rhat[3] .- sqrt.(ff) .* vz .* sum(x .* rhat)) ./ denom .- NS_vel[3]
+        # print(F[1], "\t",F[2], "\t", F[3],"\n")
+        # print(θ, "\t", ϕ,"\t", r, "\n")
+    end
+    
+    soln = nlsolve(f!, guess, autodiff = :forward, ftol=1e-24, iterations=10000)
+   
+    # FF = zeros(3)
+    # FF2 = zeros(3)
+    # f!(FF, soln.zero)
+    # f!(FF2, -soln.zero)
+    # print(FF,"\t",FF2,"\n")
+    return soln.zero
+end
+
+function test_vs_soln(θ, ϕ, r, NS_vel, x)
+    ff = sum(NS_vel.^2); # unitless
+    G = 132698000000.0 # km M_odot^-1 * (km/s)^2
+    GMr = G ./ r ./ (2.998e5 .^ 2); # unitless
+    rhat = [sin.(θ) .* cos.(ϕ) sin.(θ) .* sin.(ϕ) cos.(θ)]
+    vx, vy, vz = x
+    denom = ff .+ GMr .- sqrt.(ff) .* sum(x .* rhat);
+        
+    H1 = (ff .* vx .+ sqrt.(ff) .* GMr .* rhat[1] .- sqrt.(ff) .* vx .* sum(x .* rhat)) ./ denom .- NS_vel[1]
+    H2 = (ff .* vy .+ sqrt.(ff) .* GMr .* rhat[2] .- sqrt.(ff) .* vy .* sum(x .* rhat)) ./ denom .- NS_vel[2]
+    H3 = (ff .* vz .+ sqrt.(ff) .* GMr .* rhat[3] .- sqrt.(ff) .* vz .* sum(x .* rhat)) ./ denom .- NS_vel[3]
+    return [H1 H2 H3]
+end
+
+
+function surface_solver(Mass_a, θm, ωPul, B0, rNS, t_in, NS_vel; nsteps=10000, ln_tstart=-4, ln_tend=5, ode_err=1e-8, phiVs=100, thetaVs=100, threshold=0.05, sve=False)
+
+    
+    RT = RayTracer; # define ray tracer module
+    ConvR = RT.Find_Conversion_Surface(Mass_a, t_in, θm, ωPul, B0, rNS, thetaVs=thetaVs, phiVs=phiVs)
+    ConvR_Cut = ConvR[ConvR[:,3] .>= rNS, :]
+    NumerP = [ln_tstart, ln_tend, ode_err]
+    vGu=0.3
+    
+    finalX = zeros(2 * length(ConvR_Cut[:,1]), 3)
+    finalV = zeros(2 * length(ConvR_Cut[:,1]), 3)
+    
+    SurfaceX = zeros(2 * length(ConvR_Cut[:,1]), 3)
+    SurfaceV = zeros(2 * length(ConvR_Cut[:,1]), 3)
+    cnt = 1;
+    
+    MagnetoVars = [θm, ωPul, B0, rNS, [1.0 1.0], t_in]
+    
+    for i in 1:length(ConvR_Cut[:,1])
+        
+        θ, ϕ, r = ConvR_Cut[i,:]
+
+        
+        velV = solve_vel_CS(θ, ϕ, r, NS_vel, guess=[vGu vGu vGu])
+        velV2 = solve_vel_CS(θ, ϕ, r, NS_vel, guess=[-vGu -vGu -vGu])
+        vel_init = [velV; velV2];
+        # print(vel_init, "\n")
+        # vel_init = [velV; -1.0 .* velV];
+        # print(vel_init,"\n")
+        x_init = [r .* sin.(θ) .* cos.(ϕ) r .* sin.(θ) .* sin.(ϕ) r .* cos.(θ); r .* sin.(θ) .* cos.(ϕ) r .* sin.(θ) .* sin.(ϕ) r .* cos.(θ)]
+        xF, vF = RT.propagateAxion(x_init, vel_init, nsteps, NumerP);
+        
+   
+        if sqrt.(sum((hcat(vF[1, :, end]...) .- NS_vel) .^ 2)) ./ sqrt.(sum(NS_vel.^2)) < threshold
+            finalX[cnt, :] = xF[1, :, end];
+            finalV[cnt, :] = vF[1, :, end];
+            SurfaceX[cnt, :] = xF[1, :, 1];
+            SurfaceV[cnt, :] = vF[1, :, 1];
+            
+            
+            cnt += 1;
+        # else
+            # hold1 = test_vs_soln(θ, ϕ, r, NS_vel, velV)
+            # print(hold1,"\t");
+            # print(sqrt.(sum((hcat(vF[1, :, end]...) .- NS_vel) .^ 2)) ./ sqrt.(sum(NS_vel.^2)), "\t")
+        end
+        
+        if sqrt.(sum((hcat(vF[2, :, end]...) .- NS_vel) .^ 2)) ./ sqrt.(sum(NS_vel.^2)) < threshold
+            finalX[cnt, :] = xF[2, :, end];
+            finalV[cnt, :] = vF[2, :, end];
+            SurfaceX[cnt, :] = xF[2, :, 1];
+            SurfaceV[cnt, :] = vF[2, :, 1];
+            
+            
+            cnt += 1;
+        # else
+            # hold2 = test_vs_soln(θ, ϕ, r, NS_vel, velV2)
+            # print(hold2,"\n");
+            # print(sqrt.(sum((hcat(vF[2, :, end]...) .- NS_vel) .^ 2)) ./ sqrt.(sum(NS_vel.^2)), "\n")
+        end
+        
+    end
+    
+
+    dkdl = RT.dk_dl(SurfaceX[1:(cnt-1),:], SurfaceV[1:(cnt-1),:], [RT.ωNR_e, MagnetoVars]) ./ 6.58e-16;
+    ctheta = RT.Get_Normal_CosTheta(SurfaceX[1:(cnt-1),:], SurfaceV[1:(cnt-1),:], Mass_a, t_in, θm, ωPul, B0, rNS)
+    if sve
+        dirN = "temp_storage/"
+        fileTail = "PhaseSpace_Map_AxionM_"*string(Mass_a)*"_ThetaM_"*string(θm)*"_rotPulsar_"*string(ωPul)*"_B0_"*string(B0)*"_rNS_";
+        fileTail *= "Time_"*string(t_in)*"_sec_"
+        if NS_vel[1] != 0
+            fileTail *= "NS_velX_"*string(round(NS_vel[1], digits=4))
+        end
+        if NS_vel[2] != 0
+            fileTail *= "NS_velY_"*string(round(NS_vel[2], digits=4))
+        end
+        if NS_vel[3] != 0
+            fileTail *= "NS_velZ_"*string(round(NS_vel[3], digits=4))
+        end
+        fileTail *= "_.npz"
+        npzwrite(dirN*"SurfaceX_"*fileTail, SurfaceX[1:(cnt-1),:])
+        npzwrite(dirN*"SurfaceV_"*fileTail, SurfaceV[1:(cnt-1),:])
+        npzwrite(dirN*"FinalX_"*fileTail, finalX[1:(cnt-1),:])
+        npzwrite(dirN*"FinalV_"*fileTail, finalV[1:(cnt-1),:])
+        npzwrite(dirN*"dkdz_"*fileTail, dkdl)
+        npzwrite(dirN*"ctheta_"*fileTail, ctheta)
+    else
+        return SurfaceX[1:(cnt-1),:], SurfaceV[1:(cnt-1),:], finalX[1:(cnt-1),:], finalV[1:(cnt-1),:], dkdl, ctheta
+    end
+
+end
+
+function test_runner_surface_solver(Mass_a, θm, ωPul, B0, rNS, t_in, NS_vel; indx=3000, nsteps=10000, ln_tstart=-4, ln_tend=5, ode_err=1e-8, phiVs=100, thetaVs=100)
+
+    
+    RT = RayTracer; # define ray tracer module
+    ConvR = RT.Find_Conversion_Surface(Mass_a, t_in, θm, ωPul, B0, rNS, thetaVs=thetaVs, phiVs=phiVs)
+    NumerP = [ln_tstart, ln_tend, ode_err]
+    
+    finalX = zeros(2 * length(ConvR[:,1]), 3)
+    SurfaceX = zeros(2 * length(ConvR[:,1]), 3)
+    SurfaceV = zeros(2 * length(ConvR[:,1]), 3)
+    cnt = 1;
+    
+    θ, ϕ, r = ConvR[indx,:]
+    velV = solve_vel_CS(θ, ϕ, r, NS_vel)
+    vel_init = [velV; -velV];
+    x_init = [r .* sin.(θ) .* cos.(ϕ) r .* sin.(θ) .* sin.(ϕ) r .* cos.(θ); r .* sin.(θ) .* cos.(ϕ) r .* sin.(θ) .* sin.(ϕ) r .* cos.(θ)]
+    xF, vF = RT.propagateAxion(x_init, vel_init, nsteps, NumerP);
+  
+    return xF, vF
 
 end
 
