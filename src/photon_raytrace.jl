d1633bdd8841d6d2a971cb5ff94cc350526438f4
Samuel Witte <witte.sam@gmail.com>
2021-06-17 16:19:40 +0200

    fixing

--- src/photon_raytrace.jl
+++ src/photon_raytrace.jl
@@ -895,6 +895,7 @@ function period_average(Mass_a, Ax_g, θm, ωPul, B0, rNS, Mass_NS, t_list; ode_
         
         if !isfile(fileN)
             continue
+        end
         
         hold = npzread(fileN)
         sve_info = vcat((sve_info, hold)...)
