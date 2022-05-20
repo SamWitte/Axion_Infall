1cfa7a904139282267a110e6f78b7c005ecfcf29
Samuel Witte <witte.sam@gmail.com>
2022-05-20 19:27:10 +0200

    oops

--- src/photon_raytrace_mcmc.jl
+++ src/photon_raytrace_mcmc.jl
@@ -102,7 +102,7 @@ function func_axion!(du, u, Mvars, lnt)
         du[:,1:3] = -v .* t ;  # v is km/s, x in km, t [s]
         du[:,4:6] = GNew .* 1.0 ./ r.^2 .* xhat .* t; # units km/s/s, assume 1M NS
         if sum(r .< 10) > 0
-            du[r .< 10.0, 4:6] .= GNew .* 1.0 .* r[r .< 10.0] ./ (10.0 .^3) .* xhat[r .< 10.0] .* t[r .< 10.0]; # units km/s/s, assume 1M NS
+            du[r .< 10.0, 4:6] .= GNew .* 1.0 .* r[r .< 10.0] ./ (10.0 .^3) .* xhat[r .< 10.0] .* t; # units km/s/s, assume 1M NS
         end
         
     end
