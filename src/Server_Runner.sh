fb23a3621d8b31f6c82a8baac32b3fe2b3bf22ef
Samuel Witte <witte.sam@gmail.com>
2022-05-27 17:21:52 +0200

    fix

--- src/Server_Runner.sh
+++ src/Server_Runner.sh
@@ -10,7 +10,7 @@ hostname
 MassA=1.0e-5
 Axg=1e-12
 
-B0=1e14
+B0=1.6e14
 ThetaM=0.2
 rotW=1.67
 
@@ -31,9 +31,9 @@ memPerjob=$((SLURM_MEM_PER_NODE/SLURM_NTASKS))
 echo $memPerjob
 echo $SLURM_NTASKS
 for ((i = 0; i < $SLURM_NTASKS ; i++)); do
-    srun --ntasks=1 --exclusive --mem=$memPerjob julia --threads 1 Gen_Samples_GR_Server.jl --MassA $MassA --Axg $Axg --B0 $B0 --ThetaM $ThetaM --rotW $rotW --NS_vel_M $NS_vel_M --NS_vel_T $NS_vel_T --M_MC $M_MC --R_MC $R_MC --is_AS $is_AS --trace_trajs $trace_trajs --theta_cut_trajs $theta_cut_trajs --Nts $Trajs --ftag $tagF$i --run_RT 1 &
+    srun --ntasks=1 --exclusive --mem=$memPerjob julia --threads 1 Run_RayTracer_Server.jl --MassA $MassA --Axg $Axg --B0 $B0 --ThetaM $ThetaM --rotW $rotW --NS_vel_M $NS_vel_M --NS_vel_T $NS_vel_T --M_MC $M_MC --R_MC $R_MC --is_AS $is_AS --trace_trajs $trace_trajs --theta_cut_trajs $theta_cut_trajs --Nts $Trajs --ftag $tagF$i --run_RT 1 &
     sleep 3
 done
 wait
 
-srun --ntasks=1 --exclusive --mem=$memPerjob julia --threads 1 Gen_Samples_GR_Server.jl --MassA $MassA --Axg $Axg --B0 $B0 --ThetaM $ThetaM --rotW $rotW --NS_vel_M $NS_vel_M --NS_vel_T $NS_vel_T --M_MC $M_MC --R_MC $R_MC --is_AS $is_AS --trace_trajs $trace_trajs --theta_cut_trajs $theta_cut_trajs --Nts $Trajs --run_RT 0 --run_Combine 1 --ftag $tagF --side_runs $SLURM_NTASKS
+srun --ntasks=1 --exclusive --mem=$memPerjob julia --threads 1 Run_RayTracer_Server.jl --MassA $MassA --Axg $Axg --B0 $B0 --ThetaM $ThetaM --rotW $rotW --NS_vel_M $NS_vel_M --NS_vel_T $NS_vel_T --M_MC $M_MC --R_MC $R_MC --is_AS $is_AS --trace_trajs $trace_trajs --theta_cut_trajs $theta_cut_trajs --Nts $Trajs --run_RT 0 --run_Combine 1 --ftag $tagF --side_runs $SLURM_NTASKS
