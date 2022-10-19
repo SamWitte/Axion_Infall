46f59eb43e0a31ace9b8eee78074a7c156b88b42
Samuel Witte <witte.sam@gmail.com>
2022-10-19 10:48:37 +0200

    update test

--- src/Write_Runners_BradK.py
+++ src/Write_Runners_BradK.py
@@ -6,8 +6,8 @@ import random
 file_Load_Brad = np.loadtxt("../../../M31_encounter_data/Stripping_Models/Interaction_params_PL_M_AMC_1.00e-14_M31_youngNS_delta_1.txt")
 
 
-#MassA = 4.15e-5 # eV
-MassA = 3.51e-5 # eV
+freqRun = 8.0e9 # GHz
+MassA = 2*np.pi * freqRun * 6.58e-16 # eV
 Axg = 1e-14
 
 Num_RUN = 10
@@ -33,7 +33,6 @@ def test_plamsaF(B, P, mass):
 
 for i in range(Num_RUN):
     B0, P, ThM, Age, xx, yy, zz, MC_Den, MC_Rad, MC_Mass, b, velNS = file_Load_Brad[i, :]
-    print(Age, B0, P)
     if test_plamsaF(B0, P, MassA):
         NS_Vel_T_list.append(np.arccos(1.0 - 2.0 * random.random()))
         NS_Vel_M_list.append(velNS * 3.086e13) # km/s
@@ -50,7 +49,7 @@ for i in range(Num_RUN):
     else:
         print("fail.... \n")
         
-Trajs = 100000
+Trajs = 10000 # trajs per task
 trace_trajs = 1
 theta_cut_trajs = 1
 tagF = "_PL_"
@@ -58,26 +57,18 @@ fix_time = 0.0
 
 print("frac survive: {:.2e}".format(float(cnt) / Num_RUN))
 fileTag = "Server_Runner_"
+batchSize = 10
 
 total_runners = Num_RUN
 file_out = []
 for i in range(cnt):
 
-    file_out_HOLD = "#!/bin/bash \n"
-    file_out_HOLD += "#SBATCH --time=100:00:00 \n"
-    file_out_HOLD += "#SBATCH --mem=90G \n"
-    file_out_HOLD += "#SBATCH --nodes=1 \n"
-    file_out_HOLD += "#SBATCH --ntasks=10 \n"
-    file_out_HOLD += "#SBATCH --cpus-per-task=1 \n"
 
     file_out_HOLD += "MassA={:.2e} \n".format(MassA)
     file_out_HOLD += "Axg={:.2e} \n".format(Axg)
-
     file_out_HOLD += "B0={:.2e} \n".format(B0_List[i])
     file_out_HOLD += "ThetaM={:.2e} \n".format(ThetaM_List[i])
     file_out_HOLD += "rotW={:.2e} \n".format(rotW_List[i])
-
-
     file_out_HOLD += "NS_vel_M={:.2e} \n".format(NS_Vel_M_list[i])
     file_out_HOLD += "NS_vel_T={:.2e} \n".format(NS_Vel_T_list[i])
     file_out_HOLD += "M_MC={:.2e} \n".format(M_MC_list[i])
@@ -103,15 +94,41 @@ for i in range(cnt):
     
     file_out_HOLD += "srun --ntasks=1 --exclusive --mem=$memPerjob julia --threads 1 Run_RayTracer_Server.jl --MassA $MassA --Axg $Axg --B0 $B0 --ThetaM $ThetaM --rotW $rotW --NS_vel_M $NS_vel_M --NS_vel_T $NS_vel_T --M_MC $M_MC --R_MC $R_MC --is_AS $is_AS --trace_trajs $trace_trajs --theta_cut_trajs $theta_cut_trajs --Nts $Trajs --run_RT 0 --run_Combine 1 --ftag $tagF --side_runs $SLURM_NTASKS --fixed_time $fix_time \n"
 
+    file_out_HOLD += "wait \n"
+    
     file_out.append(file_out_HOLD)
 
 
 
-for i in range(cnt):
+for i in range(batches):
+
     fout=open(fileTag + '{}.sh'.format(i), 'w')
+    fout.write('#! /bin/bash\n')
+    fout.write('#SBATCH --mem=10G \n')
+    fout.write('#SBATCH --nodes=1 \n')
+    fout.write('#SBATCH --ntasks=10 \n')
+    fout.write('#SBATCH --cpus-per-task=1 \n')
+    
+    
+    for cmd in file_out[i::batches]:
+        fout.write('{}'.format(cmd))
+        fout.write('sleep 3 \n')
+        fout.write('wait \n')
+        
+    fout.write('wait \n')
+    fout.close()
+    cnt +=1
+    
     
     fout.write('{}\n'.format(file_out[i]))
     fout.close()
+    
+
+fout = open('simall_Anteater.sh', 'w')
+fout.write('for ((i = {:d} ; i < {:d} ; i++)); do \n'.format(1, cnt+init_indx))
+fout.write('\t sbatch Anteater_commands_"$i".sh \n')
+fout.write('done \n')
+fout.close()
 
 
 
